cfac539a308d9497eb0719962f1de826e247a4c8
Panalee <fernsat45@gmail.com>
2021-10-25 00:20:06 +0700

    mid

--- HW04_63130500075.R
+++ HW04_63130500075.R
@@ -96,16 +96,25 @@ Books %>% select(Rating,Book_title,Type) %>% filter(Rating > 4.50)
 # 1: Bar Chart
 # Graph show type of book
 ggplot(Books,aes(x = Type)) + geom_bar()
-type_plot <- ggplot(Books,aes(x = Type)) + geom_bar() + coord_flip()
-type_plot + ggtitle("Type of Book") + xlab("Types") + ylab("Number of Books")
+type_plot <- ggplot(Books,aes(x = Type)) + geom_bar() + coord_flip() + 
+             ggtitle("Type of Book") + xlab("Types") + ylab("Number of Books")
 type_plot
 
 # 2: Scatter Plot
 # Graph show relation between price and rating
-Books %>% filter(Price < 250)
-Books %>% ggplot(aes(x = Rating,y = Price)) + geom_point()
 Books %>% filter(Price < 250) %>% ggplot(aes(x = Rating,y = Price)) + geom_point()
-ratingPrice_plot <- Books %>% filter(Price < 250) %>% ggplot(aes(x = Rating,y = Price)) + geom_point(aes(color=Type)) + ggtitle("Relation Between Price and Rating")
+ratingPrice_plot <- Books %>% filter(Price < 250) %>% ggplot(aes(x = Rating,y = Price)) + 
+                    geom_point(aes(color=Type)) + ggtitle("Relation Between Price and Rating")
 ratingPrice_plot
 ratingPrice_plot + geom_smooth()
-ratingPrice_plot + geom_smooth(method="lm")
+ratingPrice_plot + geom_smooth(method="lm") + theme_minimal() + 
+                   theme(panel.grid = element_line(linetype = "dashed"))
+
+# 3: Box plot
+# Graph show Rating of the book
+rating_plot <- Books %>% ggplot(aes(x = Number_Of_Pages)) + geom_boxplot() + 
+               ggtitle("Rating of the book") + theme_light() 
+rating_plot
+
+
+
